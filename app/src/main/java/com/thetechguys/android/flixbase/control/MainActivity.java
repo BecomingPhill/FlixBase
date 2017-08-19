6bafce72911fb033caede992510a23ef22dd5c6a
tshuutheniemvula <becoming.phill@gmail.com>
2017-08-19 18:55:14 +0100

    *Added movie detail view implementation *Trying to build method in MainActivity for different END points, stuck

--- app/src/main/java/com/thetechguys/android/flixbase/control/MainActivity.java
+++ app/src/main/java/com/thetechguys/android/flixbase/control/MainActivity.java
@@ -30,7 +30,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String API_KEY = "c083761ec20e935b4194ae78e884e62e";
+    private final static String API_KEY = "KEYS FOR THE DOORS";
     private static final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView.Adapter mAdapter ;
     private List<MovieData> MovieData = new ArrayList<>();
